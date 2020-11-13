47ab740c6ce9d88fb5b7d74c8a35823b752d8213
Stan le Punk <stanislasiii@gmail.com>
2020-11-13 12:41:09 +0100

    very interesting framework !!!!

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+export default function About() {
+  return (
+    <div style={{ color: `red` }}>
+      <Header str="About Knupel Artist" />
+      <p>Knupel is Lepunk tootoo</p>
+      <img
+        src="https://raw.githubusercontent.com/StanLepunK/git_media_src/main/profil/knupel_2020_11.jpg?token=AAZNRLLSYTF3U5KV7A3K5OS7W6HAU"
+        alt=""
+      />
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>KNUPEL ART</div>
+  return (
+    <div style={{ color: `red`, fontSize: `72px` }}>
+      <Header str="KNUPEL ART" />
+      <Link to="/about/">about</Link>
+      <img
+        src="https://raw.githubusercontent.com/StanLepunK/git_media_src/main/knupel_72dpi/Atome_1_0_2015_11_450x300_72_dpi_Stan_le_Punk.jpg?token=AAZNRLLRDGVETFQF6XLYRW27W6E3S"
+        alt=""
+      />
+    </div>
+  )
 }
