a208b836838ac2afa36c4ac1202140d89ccb5acc
Stan le Punk <stanislasiii@gmail.com>
2020-11-16 10:52:24 +0100

    add media folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `https://raw.githubusercontent.com/StanLepunK/git_media_src/main/knupel_72dpi/`,
+      },
+    },
+  ],
 }
