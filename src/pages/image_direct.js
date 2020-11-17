f29e2838ac287ca695b984038ca2063417b72c5d
Stan le Punk <stanislasiii@gmail.com>
2020-11-17 17:28:32 +0100

    Revert "Revert "Revert "multi image"""
    This reverts commit db6ebd83e488960933cdcc03f162c5c45547b38f.

--- src/pages/image_direct.js
+++ src/pages/image_direct.js
@@ -1,4 +1,7 @@
 import React from "react"
+// import { Link } from "gatsby"
+// import Header from "../components/header"
+// import Container from "../components/container"
 import Layout from "../components/layout"
 export default function Galery() {
   return (
